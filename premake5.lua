ce28bbccea6b9fa2a5a40d8946dd833ebbb46552
Gordon McCann <43148247+gwm17@users.noreply.github.com>
2022-06-06 20:27:34 -0400

    Update premake5.lua to reflect glfw changes

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,12 @@ project "GLFW"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/platform.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
 	}
 	filter "system:linux"
 		pic "On"
@@ -31,6 +36,7 @@ project "GLFW"
 			"src/xkb_unicode.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
+			"src/posix_module.c",
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
@@ -54,6 +60,7 @@ project "GLFW"
             "src/cocoa_window.m",
             "src/cocoa_time.c",
             "src/posix_thread.c",
+	    "src/posix_module.c",
             "src/nsgl_context.m",
             "src/egl_context.c",
             "src/osmesa_context.c"
@@ -78,6 +85,7 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
+			"src/win32_module.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
