31670c954a574068293bc427b937681b3d921cff
Gordon McCann <43148247+gwm17@users.noreply.github.com>
2022-06-07 10:09:09 -0400

    Add posix_poll to linux build

--- premake5.lua
+++ premake5.lua
@@ -40,7 +40,8 @@ project "GLFW"
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+			"src/linux_joystick.c",
+			"src/posix_poll.c"
 		}
 
 		defines
